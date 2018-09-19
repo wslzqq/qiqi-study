b6a787a065dfbbd06035faa2fb91c090d5bd5802
wslzqq <haiyan.xu.vip@gmail.com>
2018-09-19 20:38:30 +0800

    111111

--- src/main/java/com/qiqi/study/controller/TestController.java
+++ src/main/java/com/qiqi/study/controller/TestController.java
@@ -1,12 +1,13 @@
 package com.qiqi.study.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 	@RequestMapping("/hello")
-    public String index(@RequestParams(value = '')) {
+    public String index(@RequestParam(value = "test") String test) {
         return "Hello World";
     }
 }
